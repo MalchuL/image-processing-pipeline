66dff796c4433b5e3af3ab3dca8114350dbefecc
malchul <vladislaw.sorockin@yandex.ru>
2023-01-24 22:37:02 +0300

    Added setup.py script

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='image-processing-pipeline',
+    name='image_processing_pipeline',
     version='',
     packages=['utils', 'pipeline',
               'pipeline.utils', 'pipeline.readers', 'pipeline.detectors', 'pipeline.detectors.lib',
@@ -12,5 +12,15 @@ setup(
     license='',
     author='malchul',
     author_email='',
-    description=''
+    description='',
+    install_requires=['numpy==1.24.0',
+                     ' tqdm==4.19.9',
+                     ' omegaconf',
+                     ' opencv-python==4.5.5.64',
+                     ' opencv-contrib-python==4.5.5.64',
+                      # -------- infer ------------- #
+                     'mediapipe',
+                     'onnxruntime',
+                     ],
+    python_requires='>=3.6',
 )
